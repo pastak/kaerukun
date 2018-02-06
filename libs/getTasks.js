eb2deb899534e0f0e1887468abe73b3530303045
pastak <pasta0915@gmail.com>
2018-02-06 18:40:24 +0900

    Parse group recursive

--- libs/getTasks.js
+++ libs/getTasks.js
@@ -4,45 +4,96 @@ const uniq = require('array-unique');
 
 const mappingTasks = (url, tasks) => {
   if (!tasks) return [];
-  return tasks.map(filePath => {return {filePath, url};});
+  if (Object.keys(tasks).length === 0) return [];
+  return tasks.filter(_ => _).map(filePath => {return {filePath, url};});
 };
 
 module.exports = (config) => {
-  const {tasks, urls} = config;
+  let {tasksDir, tasks, urls} = config;
+  tasksDir = tasks;
   let taskGroupList = {};
 
-  const getTasksInDir = (dirPath) => {
+  const getTasksInDir = (dirPath, noRecursive = false) => {
     return fs.readdirSync(path.resolve(dirPath))
-      .filter(_ => fs.statSync(path.resolve(dirPath, _)).isFile())
-      .map(fileName => path.join(dirPath, fileName));
+      .map(fileName => {
+        if (fs.statSync(path.resolve(dirPath, fileName)).isFile()) {
+          return path.join(dirPath, fileName);
+        } else {
+          if (noRecursive) return;
+          let t = {}
+          t[fileName] = getTasksInDir(path.join(dirPath, fileName));
+          return t;
+        }
+      });
   };
 
-  const tasksForAll = getTasksInDir(tasks);
+  const tasksForAll = getTasksInDir(tasksDir, true);
 
-  fs.readdirSync(path.resolve(tasks))
-    .filter(_ => fs.statSync(path.resolve(tasks, _)).isDirectory())
+  fs.readdirSync(path.resolve(tasksDir))
     .forEach((group) => {
-      taskGroupList[group] = getTasksInDir(path.join(tasks, group));
+      if (fs.statSync(path.resolve(tasksDir, group)).isFile()) return;
+      taskGroupList[group] = getTasksInDir(path.join(tasksDir, group));
     });
 
-  let result = [];
-  let _urls = [];
-  if (Array.isArray(urls)) {
-    urls.forEach((item) => {
-      if (typeof item === 'string') {
-        _urls.push(item);
+  // make TaskList flatten
+
+  let flatTaskList = {};
+  const flattenTaskList = (obj, prefix = '') => {
+    if (!obj) return;
+    Object.keys(obj).forEach(key => {
+      const value = obj[key];
+      if (typeof value === 'string') {
+        flatTaskList[prefix + key] = value;
+        return;
+      } else if (Array.isArray(value)) {
+        flatTaskList[prefix + key] = [];
+        value.forEach(val => {
+          if (typeof val === 'string') {
+            flatTaskList[prefix + key].push(val)
+          } else if (typeof val === 'object') {
+            flattenTaskList(val, prefix + key + '/');
+          }
+        });
       }
-    });
-  } else {
-    Object.keys(urls).forEach((group) => {
-      urls[group].forEach((url) => {
-        _urls.push(url);
-        result.push(...mappingTasks(url, taskGroupList[group]));
-      });
-    });
+    })
   }
-  uniq(_urls).forEach((url) => {
-    result.push(...mappingTasks(url, tasksForAll));
+  flattenTaskList(taskGroupList);
+
+  let allResult = [];
+  let allUrls = [];
+
+  const recursiveMapping = (urls, prefix = '') => {
+    if (Array.isArray(urls)) {
+      allUrls.push(...urls);
+      return mappingTasks(urls, flatTaskList);
+    } else {
+      let result = [];
+      Object.keys(urls).forEach((group) => {
+        urls[group].forEach((url) => {
+          if (typeof url === 'string') {
+            allUrls.push(url);
+            const targetTasks = [].concat(
+              ...Object.keys(flatTaskList)
+                .filter(
+                  key => (new RegExp('^' + key)).test(prefix + group)
+                )
+                .map(key => flatTaskList[key])
+            )
+            result.push(...mappingTasks(url, targetTasks));
+          } else {
+            result.push(...recursiveMapping(url, group + '/'));
+          }
+        });
+      });
+      return result;
+    }
+  }
+
+  allResult.push(...recursiveMapping(urls));
+
+  uniq(allUrls).filter(_ => typeof _ === 'string').forEach((url) => {
+    allResult.push(...mappingTasks(url, tasksForAll));
   });
-  return result;
+
+  return  [...new Set(allResult.map(o => JSON.stringify(o)))].map(s => JSON.parse(s))
 };
--- test/libs/getTasks.js
+++ test/libs/getTasks.js
@@ -47,3 +47,48 @@ test('grouping', t => {
     {filePath: 'test/fixtures/tasks/grouping/all.js', url: 'https://example.com/baz'},
   ]);
 });
+
+test('recursive grouping', t => {
+  const config = {
+    tasks: './test/fixtures/tasks/r-grouping',
+    urls: {
+      'a': [
+        {
+          'a': [
+            'https://example.com/foo',
+            'https://example.com/bar'
+          ],
+          'b': [
+            'https://example.com/bar'
+          ],
+          'c': [
+            'https://example.com/baz'
+          ]
+        },
+        'https://example.com/abaz'
+      ],
+      'b': [
+        'https://example.com/bbaz'
+      ]
+    }
+  };
+  const actual = getTasks(config);
+  t.deepEqual(actual, [
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a-all.js', url: 'https://example.com/foo'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a/fuga.js', url: 'https://example.com/foo'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a/hoge.js', url: 'https://example.com/foo'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a-all.js', url: 'https://example.com/bar'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a/fuga.js', url: 'https://example.com/bar'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a/hoge.js', url: 'https://example.com/bar'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/b/fuga.js', url: 'https://example.com/bar'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/b/hoge.js', url: 'https://example.com/bar'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a-all.js', url: 'https://example.com/baz'},
+    {filePath: 'test/fixtures/tasks/r-grouping/a/a-all.js', url: 'https://example.com/abaz'},
+    {filePath: 'test/fixtures/tasks/r-grouping/b/fuga.js', url: 'https://example.com/bbaz'},
+    {filePath: 'test/fixtures/tasks/r-grouping/all.js', url: 'https://example.com/foo'},
+    {filePath: 'test/fixtures/tasks/r-grouping/all.js', url: 'https://example.com/bar'},
+    {filePath: 'test/fixtures/tasks/r-grouping/all.js', url: 'https://example.com/baz'},
+    {filePath: 'test/fixtures/tasks/r-grouping/all.js', url: 'https://example.com/abaz'},
+    {filePath: 'test/fixtures/tasks/r-grouping/all.js', url: 'https://example.com/bbaz'},
+  ]);
+});
